4065311b12a2a5b556881ca11f682589181cc29b
Jt-Gutierrez-W <71188483+Jt-Gutierrez-W@users.noreply.github.com>
2021-08-02 12:56:15 -0400

    first one

--- timing_matmul.py
+++ timing_matmul.py
@@ -1,20 +1,17 @@
-from numpy import zeros
+from numpy import zeros 
 from time import perf_counter
 
+#tamaño
 
-
-#Tamaño
 N = 1000
-A = zeros((N, N))+1
-B = zeros((N, N))+2
-
+A = zeros((N,N))+1
+B = zeros((N,N))+2
 
 t1 = perf_counter()
 C = A@B
 t2 = perf_counter()
 
-
-dt = t2 - t1
+dt = t2-t1
 
 print(f"dt = {dt} s")
 
