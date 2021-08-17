d63733d7e3c60ab96848bcd42745772ec486cad3
Jt-Gutierrez-W <71188483+Jt-Gutierrez-W@users.noreply.github.com>
2021-08-17 14:05:48 -0400

    Add files via upload

---
+++ python_extensions/Graf_inv.py
@@ -0,0 +1,65 @@
+
+import matplotlib.pyplot as plt
+
+
+
+
+time = []; size = []; memory = [];
+time_buffer = []; size_buffer = []; memory_buffer = [];
+file = open("output_file_timing_inv_caso3_longdouble.txt","r") #reemplazar nombre del archivo y Title para generar nuevo grafico
+lineas=file.readlines()
+for linea in lineas:
+    buffer =list(linea.split())
+    
+    if len(buffer) == 6:
+        pass
+    if (len(buffer) == 2 and buffer[1] != '1' and len(time_buffer)!=0) or buffer[0] == "EOF":
+        
+        time.append(time_buffer); size.append(size_buffer); memory.append(memory_buffer);
+        memory_buffer = []; size_buffer = []; time_buffer = []
+    if len (buffer) == 3:
+        time_buffer.append(float(buffer[0])); memory_buffer.append(float(buffer[1])); size_buffer.append(float(buffer[2]))
+        
+
+        
+x = [10,20,50,100,200,500,1000,2000,5000,10000,20000]
+y_time = [1e-4, 0.001, 0.01, 0.1, 1, 10, 60, 600] #ticks
+y_labels = ["0.1 ms", "1 ms", "10 ms", "0.1 s", "1 s", "10 s", "1 min", "10 min"]
+y_memory = [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
+y_mlabels = ["1 KB", "10 KB", "100 KB", "1 MB", "10 MB", "100 MB", "1 GB", "10 GB"]
+plt.figure(1)
+
+
+plt.subplot(2,1,1)
+plt.title("Rendimiento Caso 3 Longdouble")
+plt.ylabel("Tiempo transcurrido (s)")
+for run in time:
+    plt.loglog(size[0],run)
+    plt.plot(size[0],run,marker='o')
+plt.yticks(y_time, y_labels)
+plt.xticks(x,[],rotation = 70)
+plt.grid()
+
+
+
+
+plt.subplot(2,1,2)
+plt.xlabel("Tamaño matriz N")
+plt.ylabel("Uso memoria (s)")
+plt.axhline(y=16000, color='black', linestyle='--')
+
+
+
+for corun in memory:
+    plt.loglog(size[0],corun)
+    plt.scatter(size[0],corun)
+    plt.plot(size[0],corun)
+
+plt.yticks(y_memory,y_mlabels)
+plt.xticks(x,x,rotation = 70)
+plt.grid()
+
+
+
+plt.show()
+file.close()
---
+++ python_extensions/timing_inv_caso2_double.py
@@ -0,0 +1,66 @@
+
+import numpy as np
+from numpy.linalg import inv
+from time import perf_counter
+from numpy import geomspace
+from scipy import linalg
+
+def generar_matriz (N,dtype):
+    L=np.zeros((N,N),dtype=dtype)
+    np.fill_diagonal(L, dtype(2))
+    b= -np.ones(N-1,dtype=dtype)
+    np.fill_diagonal(L[1:], b)
+    np.fill_diagonal(L[:,1:], b)
+    return L
+
+
+def memory_time_numpy (N,dtype): # numpy inverse
+    L = generar_matriz(N, dtype)
+    t1 = perf_counter()
+    I = inv(L)
+    t2 = perf_counter()
+    malloc = L.nbytes + I.nbytes
+    dt = t2-t1
+    return dt, malloc/(10**6)
+    
+
+def memory_time_scipy_f (N,dtype): #scipy inv overwrite_a=False
+    L = generar_matriz(N, dtype)
+    t1 = perf_counter()
+    I = linalg.inv(L,overwrite_a=False)
+    t2 = perf_counter()
+    malloc = L.nbytes + I.nbytes
+    dt = t2-t1
+    return dt, malloc/(10**6)
+    
+
+def memory_time_scipy_t (N,dtype): #scipy inv overwrite_a=True
+    L = generar_matriz(N, dtype)
+    t1 = perf_counter()
+    I = linalg.inv(L,overwrite_a=True)
+    t2 = perf_counter()
+    malloc = L.nbytes + I.nbytes
+    dt = t2-t1
+    return dt, malloc/(10**6)
+
+
+output_file = open("output_file_timing_inv_caso2_double.txt","w")
+output_file.write("Tiempo [segundos] Memoria [MB] Tamaño [N]\n") #header of the txt file
+
+for corridas in range(1,10+1):
+    output_file.write(f"Corrida {corridas}:\n")
+    anterior = False
+    for i in geomspace(1, 20_000,20):
+        actual = int(i)
+        if anterior == actual:
+            pass
+        else:
+            dt, malloc = memory_time_scipy_f(int(i),np.double)
+            print(f" N: {int(i)} ---> {dt:.6f} segundos  {malloc:.6f} MB")
+            output_file.write(f"  {dt:.6f}         {malloc:.6f}         {int(i)} \n")
+        anterior = actual
+        
+
+
+output_file.write("EOF\n")
+output_file.close ()
